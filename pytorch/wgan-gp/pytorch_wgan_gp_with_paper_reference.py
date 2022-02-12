5f31d68d322106035c80c89b21fba78730fd8e78
Asif Ahmed <public.quickgrid@gmail.com>
2022-02-12 19:12:09 +0600

    fix issue #2, added checkpoint saving and loading

--- pytorch/wgan-gp/pytorch_wgan_gp_with_paper_reference.py
+++ pytorch/wgan-gp/pytorch_wgan_gp_with_paper_reference.py
@@ -20,12 +20,13 @@ References:
 """
 
 import os
+import pathlib
+from typing import Tuple
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchvision
-import torchvision.datasets as datasets
 from torchvision.transforms import transforms
 from torch.utils.data import DataLoader, Dataset
 from torch.utils.tensorboard import SummaryWriter
@@ -33,20 +34,29 @@ from PIL import Image
 
 
 class Critic(nn.Module):
-    def __init__(self, img_channels, feature_map_base):
+    def __init__(
+            self,
+            img_channels: int,
+            feature_map_base: int,
+    ) -> None:
         super(Critic, self).__init__()
 
-        def _blocks(in_channels, out_channels, kernel_size, stride, padding):
+        def _blocks(
+                in_channels: int,
+                out_channels: int,
+                kernel_size: Tuple[int, int],
+                stride: Tuple[int, int],
+                padding: Tuple[int, int],
+        ) -> nn.Sequential:
             return nn.Sequential(
                 nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=False),
-                nn.LayerNorm(out_channels),
+                nn.InstanceNorm2d(out_channels, affine=True),
                 nn.LeakyReLU(0.2)
             )
 
         k_size = (4, 4)
         s_amount = (2, 2)
         p_amount = (1, 1)
-
         self.C = nn.Sequential(
             nn.Conv2d(img_channels, feature_map_base, k_size, s_amount, p_amount),
             nn.LeakyReLU(0.2),
@@ -56,15 +66,26 @@ class Critic(nn.Module):
             nn.Conv2d(feature_map_base * 8, 1, k_size, s_amount, padding=(0, 0))
         )
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.C(x)
 
 
 class Generator(nn.Module):
-    def __init__(self, z_dim, img_channels, feature_map_base):
+    def __init__(
+            self,
+            z_dim: int,
+            img_channels: int,
+            feature_map_base: int,
+    ) -> None:
         super(Generator, self).__init__()
 
-        def _blocks(in_channels, out_channels, kernel_size, stride, padding):
+        def _blocks(
+                in_channels: int,
+                out_channels: int,
+                kernel_size: Tuple[int, int],
+                stride: Tuple[int, int],
+                padding: Tuple[int, int],
+        ) -> nn.Sequential:
             return nn.Sequential(
                 nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding, bias=False),
                 nn.BatchNorm2d(out_channels),
@@ -74,7 +95,6 @@ class Generator(nn.Module):
         k_size = (4, 4)
         s_amount = (2, 2)
         p_amount = (1, 1)
-
         self.G = nn.Sequential(
             *_blocks(z_dim, feature_map_base * 16, k_size, stride=(1, 1), padding=(0, 0)),
             *_blocks(feature_map_base * 16, feature_map_base * 8, k_size, s_amount, p_amount),
@@ -89,7 +109,12 @@ class Generator(nn.Module):
 
 
 class CustomImageDataset(Dataset):
-    def __init__(self, root_dir, image_size, image_channels):
+    def __init__(
+            self,
+            root_dir: str,
+            image_size: int,
+            image_channels: int,
+    ) -> None:
         super(CustomImageDataset, self).__init__()
         self.root_dir = root_dir
         self.images = os.listdir(root_dir)
@@ -103,42 +128,42 @@ class CustomImageDataset(Dataset):
             )
         ])
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.images)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx: int) -> torch.Tensor:
         image_path = os.path.join(self.root_dir, self.images[idx])
         image = Image.open(image_path)
         return self.transform(image)
 
 
-class Trainer():
+class Trainer:
     def __init__(
             self,
-            root_dir='',
-            device=torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
-            num_workers=0,
-            batch_size=64,
-            image_size=64,
-            image_channels=3,
-            num_epochs=10,
-            z_dim=100,
-            learning_rate=1e-4,
-            generator_feature_map_base=64,
-            critic_feature_map_base=64,
-            critic_iterations=5,
-            lambda_gp=10,
-            *args,
-            **kwargs
-    ):
+            root_dir: str,
+            device: torch.device,
+            checkpoint_path: str = None,
+            save_checkpoint_every: int = 100,
+            num_workers: int = 0,
+            batch_size: int = 64,
+            image_size: int = 64,
+            image_channels: int = 3,
+            num_epochs: int = 1000,
+            z_dim: int = 100,
+            learning_rate: float = 1e-4,
+            generator_feature_map_base: int = 64,
+            critic_feature_map_base: int = 64,
+            critic_iterations: int = 2,
+            lambda_gp: int = 10,
+    ) -> None:
         super(Trainer, self).__init__()
-
-        self.NUM_EPOCHS = num_epochs
-        self.DEVICE = device
-        self.CRITIC_ITERATIONS = critic_iterations
-        self.LAMBDA_GP = lambda_gp
+        self.num_epochs = num_epochs
+        self.device = device
+        self.critic_iterations = critic_iterations
+        self.lambda_gp = lambda_gp
         self.BATCH_SIZE = batch_size
-        self.Z_DIM = z_dim
+        self.z_dim = z_dim
+        self.save_checkpoint_every = save_checkpoint_every
 
         gan_dataset = CustomImageDataset(root_dir=root_dir, image_size=image_size, image_channels=image_channels)
         self.train_loader = DataLoader(gan_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
@@ -148,8 +173,17 @@ class Trainer():
                 if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
                     nn.init.normal_(m.weight.data, mean=mean, std=std)
 
-        self.G = Generator(z_dim=z_dim, img_channels=image_channels, feature_map_base=generator_feature_map_base).to(self.DEVICE)
-        self.C = Critic(img_channels=image_channels, feature_map_base=critic_feature_map_base).to(self.DEVICE)
+        self.G = Generator(
+            z_dim=z_dim,
+            img_channels=image_channels,
+            feature_map_base=generator_feature_map_base
+        ).to(self.device)
+
+        self.C = Critic(
+            img_channels=image_channels,
+            feature_map_base=critic_feature_map_base
+        ).to(self.device)
+
         _initialize_weights(self.G)
         _initialize_weights(self.C)
 
@@ -167,9 +201,18 @@ class Trainer():
         self.writer_fake = SummaryWriter(f"logs/fake")
         self.step = 0
 
-    def get_gradient_penalty(self, real, fake):
-        BATCH_SIZE, C, H, W = real.shape
-        epsilon = torch.rand((BATCH_SIZE, 1, 1, 1), device=self.DEVICE).repeat(1, C, H, W)
+        self.start_epoch = 0
+        pathlib.Path('checkpoints').mkdir(parents=True, exist_ok=True)
+        if checkpoint_path is not None:
+            self.load_checkpoint(checkpoint_path=checkpoint_path)
+
+    def get_gradient_penalty(
+            self,
+            real: torch.Tensor,
+            fake: torch.Tensor
+    ) -> torch.Tensor:
+        batch_size, c, h, w = real.shape
+        epsilon = torch.rand((batch_size, 1, 1, 1), device=self.device).repeat(1, c, h, w)
         interpolated_images = real * epsilon + fake * (1 - epsilon)
 
         mixed_scores = self.C(interpolated_images)
@@ -185,28 +228,38 @@ class Trainer():
         gradient_penalty = torch.mean((gradient_norm - 1) ** 2)
         return gradient_penalty
 
-    def train(self):
-        for epoch in range(self.NUM_EPOCHS):
+    def load_checkpoint(self, checkpoint_path: str) -> None:
+        checkpoint = torch.load(checkpoint_path)
+        self.G.load_state_dict(checkpoint['generator_state_dict'])
+        self.opt_G.load_state_dict(checkpoint['generator_optimizer_state_dict'])
+        self.C.load_state_dict(checkpoint['critic_state_dict'])
+        self.opt_C.load_state_dict(checkpoint['critic_optimizer_state_dict'])
+        self.start_epoch = checkpoint['epoch']
+
+    def train(self) -> None:
+        for epoch in range(self.start_epoch, self.num_epochs):
             for batch_idx, (real) in enumerate(self.train_loader):
-                real = real.to(self.DEVICE)
+                real = real.to(self.device)
                 current_batch_size = real.shape[0]
 
                 # Critic optimization.
                 mean_iteration_critic_loss = 0
-                for _ in range(self.CRITIC_ITERATIONS):
-                    noise = torch.randn((current_batch_size, self.Z_DIM, 1, 1), device=self.DEVICE)
+                for _ in range(self.critic_iterations):
+                    noise = torch.randn((current_batch_size, self.z_dim, 1, 1), device=self.device)
                     fake = self.G(noise)
                     critic_real = self.C(real).reshape(-1)
                     critic_fake = self.C(fake.detach()).reshape(-1)
-                    gradient_penalty = self.get_gradient_penalty(real=critic_real, fake=critic_fake)
-                    loss_critic = -(torch.mean(critic_real) - torch.mean(critic_fake)) + self.LAMBDA_GP * gradient_penalty
-                    mean_iteration_critic_loss += loss_critic.item() / self.CRITIC_ITERATIONS
+                    gradient_penalty = self.get_gradient_penalty(real=real, fake=fake)
+                    loss_critic = (
+                            -(torch.mean(critic_real) - torch.mean(critic_fake)) + self.lambda_gp * gradient_penalty
+                    )
+                    mean_iteration_critic_loss += loss_critic.item() / self.critic_iterations
                     self.opt_C.zero_grad()
                     loss_critic.backward()
                     self.opt_C.step()
 
                 # Generator optimization.
-                noise = torch.randn((current_batch_size, self.Z_DIM, 1, 1), device=self.DEVICE)
+                noise = torch.randn((current_batch_size, self.z_dim, 1, 1), device=self.device)
                 fake = self.G(noise)
                 output = self.C(fake).reshape(-1)
                 loss_gen = -torch.mean(output)
@@ -214,11 +267,19 @@ class Trainer():
                 loss_gen.backward()
                 self.opt_G.step()
 
-                if batch_idx % 100 == 0:
+                if batch_idx % self.save_checkpoint_every == 0:
+                    torch.save({
+                        'epoch': epoch,
+                        'generator_state_dict': self.G.state_dict(),
+                        'critic_state_dict': self.C.state_dict(),
+                        'generator_optimizer_state_dict': self.opt_G.state_dict(),
+                        'critic_optimizer_state_dict': self.opt_C.state_dict(),
+                    }, f'checkpoints/checkpoint_{epoch}.pt')
+
                     self.G.eval()
                     self.C.eval()
                     print(
-                        f"EPOCH: [{epoch} / {self.NUM_EPOCHS}], BATCH: [{batch_idx} / {len(self.train_loader)}], "
+                        f"EPOCH: [{epoch} / {self.num_epochs}], BATCH: [{batch_idx} / {len(self.train_loader)}], "
                         f"LOSS(Mean) D: {mean_iteration_critic_loss:.4f}, LOSS G: {loss_gen:.4f}"
                     )
                     with torch.no_grad():
@@ -232,31 +293,10 @@ class Trainer():
                     self.C.train(True)
 
 
-class GANUtils():
-    def __init__(self):
-        super(GANUtils, self).__init__()
-        self.model_args = dict(
-            root_dir='C:\\Users\\computer\Desktop\\celeba gan\\celeba_hq_dataset\\images',
-            device=torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
-            NUM_WORKERS=0,
-            BATCH_SIZE=64,
-            IMAGE_SIZE=64,
-            IMAGE_CHANNELS=3,
-            NUM_EPOCHS=10,
-            Z_DIM=100,
-            LEARNING_RATE=1e-4,
-            GENERATOR_FEATURE_MAP_BASE=64,
-            CRITIC_FEATURE_MAP_BASE=64,
-            CRITIC_ITERATIONS=5,
-            LAMBDA_GP=0.01
-        )
-
-    def get_train_config(self, debug=True):
-        if debug:
-            print(self.model_args)
-        return self.model_args
-
-
 if __name__ == '__main__':
-    trainer = Trainer(**(GANUtils().get_train_config()))
+    trainer = Trainer(
+        root_dir=r'C:\data\celeba',
+        device=torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
+        # checkpoint_path='checkpoints/checkpoint_12.pt'
+    )
     trainer.train()
