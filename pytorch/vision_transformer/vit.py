1f6c4a2460a4425d31bbc8bd48ab212c01c0be46
Asif Ahmed <public.quickgrid@gmail.com>
2022-02-18 02:09:41 +0600

    Update vit.py

--- pytorch/vision_transformer/vit.py
+++ pytorch/vision_transformer/vit.py
@@ -1,17 +1,16 @@
 """Implementation of Vision Transformer (ViT) in pytorch.
 
 TODO
-    - Add learnable class token embedding.
     - Remove `drop_last` for datasets.
-    - Add accuracy logic print and in tensorboard.
-    - Add loss data to tensorboard.
-    - Add train validation split with validation loss.
+    - Add accuracy logic print, loss data in tensorboard.
+    - Replace with einops.
+    - Format output better.
 
 References
-    - https://keras.io/examples/vision/image_classification_with_vision_transformer/.
-    - https://github.com/lucidrains/vit-pytorch/blob/main/vit_pytorch/vit.py.
-    - https://github.com/jeonsworld/ViT-pytorch/blob/main/models/modeling.py.
-    - https://docs.microsoft.com/en-us/windows/ai/windows-ml/tutorials/pytorch-analysis-train-model.
+    - https://keras.io/examples/vision/image_classification_with_vision_transformer/
+    - https://github.com/lucidrains/vit-pytorch/blob/main/vit_pytorch/vit.py
+    - https://github.com/jeonsworld/ViT-pytorch/blob/main/models/modeling.py
+    - https://docs.microsoft.com/en-us/windows/ai/windows-ml/tutorials/pytorch-analysis-train-model
 """
 
 import os
@@ -25,6 +24,7 @@ from matplotlib import pyplot as plt
 from torch.utils.data import Dataset, DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from torchvision.transforms import transforms
+from tqdm import tqdm
 
 
 class VisionTransformerDataset(Dataset):
@@ -36,7 +36,6 @@ class VisionTransformerDataset(Dataset):
     ) -> None:
         super(VisionTransformerDataset, self).__init__()
         _class_list = os.listdir(root_dir)
-        print(_class_list)
 
         self.transform = transforms.Compose([
             transforms.Resize((image_size, image_size)),
@@ -110,13 +109,13 @@ class PatchPositionEncoder(nn.Module):
     ) -> None:
         super(PatchPositionEncoder, self).__init__()
         self.patch_embeddings = nn.LazyLinear(out_features=embedding_dim)
-        self.position_embeddings = nn.Parameter(torch.zeros(1, num_patches, embedding_dim))
-        # self.class_token = nn.Parameter(torch.randn(1, 1, embedding_dim))
+        self.position_embeddings = nn.Parameter(torch.zeros(1, num_patches + 1, embedding_dim))
+        self.class_token = nn.Parameter(torch.randn(1, 1, embedding_dim))
 
     def forward(self, patch: torch.Tensor) -> torch.Tensor:
-        patch_e = self.patch_embeddings(patch)
-        pos_e = self.position_embeddings
-        encoding = patch_e + pos_e
+        class_token = self.class_token.expand(patch.shape[0], -1, -1)
+        class_token_patch_embedding = torch.cat([class_token, self.patch_embeddings(patch)], dim=1)
+        encoding = class_token_patch_embedding + self.position_embeddings
         return encoding
 
 
@@ -129,7 +128,7 @@ class ClassificationMLP(nn.Module):
     ) -> None:
         super(ClassificationMLP, self).__init__()
         self.flatten = nn.Flatten()
-        self.class_mapping_head = nn.Linear(in_features=embedding_dim * patch_count, out_features=num_classes)
+        self.class_mapping_head = nn.Linear(in_features=embedding_dim * (patch_count + 1), out_features=num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.flatten(x)
@@ -144,8 +143,12 @@ class VisionTransformerModel(nn.Module):
             embedding_dim: int,
             transformer_layers_count: int,
             num_patches: int,
+            patch_count: int,
+            patch_size: int,
     ) -> None:
         super(VisionTransformerModel, self).__init__()
+        self.patch_count = patch_count
+        self.patch_size = patch_size
 
         _patch_encoder = PatchPositionEncoder(
             num_patches=num_patches,
@@ -160,8 +163,15 @@ class VisionTransformerModel(nn.Module):
 
         self.vision_transformer_layers = nn.Sequential(*_vision_transformer_modules)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.vision_transformer_layers(x)
+    def forward(self, img: torch.Tensor) -> torch.Tensor:
+        with torch.no_grad():
+            # (N C IMG_H IMG_W) -> (N C PATCH_COUNT_H PATCH_COUNT_W PATCH_SIZE_H PATCH_SIZE_W)
+            # (64 3 72 72) -> (64 3 12 12 6 6) -> (64 12 12 6 6 3) -> (64 144 108)
+            patches = img.unfold(2, self.patch_size, self.patch_size).unfold(3, self.patch_size, self.patch_size)
+            patches = patches.permute(0, 2, 3, 4, 5, 1)
+            patches = patches.reshape(img.shape[0], self.patch_count, -1)  # TODO: torch.view
+
+        x = self.vision_transformer_layers(patches)
         return x
 
 
@@ -171,6 +181,7 @@ class Trainer:
             dataset_path: str,
             checkpoint_path: str = None,
             device: str = None,
+            train_split_percentage: float = 0.2,
             num_epochs: int = 1000,
             batch_size: int = 64,
             save_every: int = 20,
@@ -191,20 +202,33 @@ class Trainer:
         self.patch_size = patch_size
 
         self.patch_count = (image_size // patch_size) ** 2
-        num_classes = len(os.listdir(dataset_path))
+        class_names = os.listdir(dataset_path)
+        num_classes = len(class_names)
 
         vit_dataset = VisionTransformerDataset(
             root_dir=dataset_path,
             image_channels=image_channels,
             image_size=image_size,
         )
+        train_size = int(train_split_percentage * len(vit_dataset))
+        test_size = len(vit_dataset) - train_size
+        train_dataset, validation_dataset = torch.utils.data.random_split(vit_dataset, [train_size, test_size])
+
         self.train_loader = DataLoader(
-            dataset=vit_dataset,
+            dataset=train_dataset,
             batch_size=batch_size,
             shuffle=True,
             num_workers=num_workers,
             pin_memory=True,
             drop_last=True,  # Unable to train without this why?
+        )
+        self.validation_loader = DataLoader(
+            dataset=validation_dataset,
+            batch_size=batch_size,
+            shuffle=False,
+            num_workers=num_workers,
+            pin_memory=True,
+            drop_last=True,
         )
 
         self.vit_model = VisionTransformerModel(
@@ -212,6 +236,8 @@ class Trainer:
             embedding_dim=patch_embedding_dim,
             transformer_layers_count=transformer_layers_count,
             num_patches=self.patch_count,
+            patch_size=patch_size,
+            patch_count=self.patch_count,
         )
         self.mlp_head = ClassificationMLP(
             num_classes=num_classes,
@@ -244,20 +270,18 @@ class Trainer:
         self.start_epoch = checkpoint['epoch']
 
     def train(self) -> None:
+        best_accuracy = 0.0
         for epoch in range(self.start_epoch, self.num_epochs):
             # Training loop.
             running_train_loss = 0.0
-            for idx, (img, labels) in enumerate(self.train_loader):
+            training_correct_preds = 0
+            training_total_data = 0
+            tqdm_train_loader = tqdm(self.train_loader)
+            for idx, (img, labels) in enumerate(tqdm_train_loader):
                 img = img.to(self.device)
                 labels = labels.to(self.device)
 
-                # (N C IMG_H IMG_W) -> (N C PATCH_COUNT_H PATCH_COUNT_W PATCH_SIZE_H PATCH_SIZE_W)
-                # (64 3 72 72) -> (64 3 12 12 6 6) -> (64 12 12 6 6 3) -> (64 144 108)
-                patches = img.unfold(2, self.patch_size, self.patch_size).unfold(3, self.patch_size, self.patch_size)
-                patches = patches.permute(0, 2, 3, 4, 5, 1)
-                patches = patches.reshape(self.batch_size, self.patch_count, -1)  # TODO: torch.view
-
-                vit_output = self.vit_model(patches)
+                vit_output = self.vit_model(img)
                 predicted_labels = self.mlp_head(vit_output)
 
                 self.optim.zero_grad()
@@ -266,32 +290,50 @@ class Trainer:
                 self.optim.step()
 
                 running_train_loss += loss.item()
+                training_total_data += img.shape[0]
+                training_correct_preds += (torch.argmax(predicted_labels, dim=1) == labels).sum().item()
 
-                with torch.no_grad():
+                tqdm_train_loader.set_description(
+                    f'TRAIN EPOCH: {epoch} '
+                )
+
+            train_loss_value = running_train_loss / len(self.train_loader)
+            training_accuracy = (100.0 * training_correct_preds / training_total_data)
+
+            # Validation loop.
+            running_validation_loss = 0.0
+            validation_correct_preds = 0
+            validation_total_data = 0
+            with torch.no_grad():
+                self.vit_model.eval()
+                self.mlp_head.eval()
+                tqdm_validation_loader = tqdm(self.validation_loader)
+                for idx, (img, labels) in enumerate(tqdm_validation_loader):
+                    img = img.to(self.device)
+                    labels = labels.to(self.device)
+
+                    vit_output = self.vit_model(img)
+                    predicted_labels = self.mlp_head(vit_output)
+
+                    validation_loss = self.loss_fn(predicted_labels, labels)
+
+                    running_validation_loss += validation_loss.item()
+                    validation_total_data += img.shape[0]
+                    validation_correct_preds += (torch.argmax(predicted_labels, dim=1) == labels).sum().item()
+
+                    tqdm_validation_loader.set_description(
+                        f'VALID EPOCH: {epoch} '
+                    )
+
                     if idx % self.save_every == self.save_every - 1:
-                        torch.save({
-                            'epoch': epoch,
-                            'vit_model_state_dict': self.vit_model.state_dict(),
-                            'mlp_head_state_dict': self.mlp_head.state_dict(),
-                            'optimizer_state_dict': self.optim.state_dict(),
-                        }, f'checkpoints/checkpoint_{epoch}.pt')
-
-                        self.vit_model.eval()
-                        self.mlp_head.eval()
-
-                        print(
-                            f"EPOCH: [{epoch + 1} / {self.num_epochs}], BATCH: [{idx + 1} / {len(self.train_loader)}],",
-                            f"LOSS: {running_train_loss / self.save_every:.4f}",
-                        )
-                        running_train_loss = 0.0
-
                         fig, ax = plt.subplots(nrows=self.nrows, ncols=self.ncols)
                         plt.setp(plt.gcf().get_axes(), xticks=[], yticks=[])
                         k = 0
                         for i in range(self.nrows):
                             for j in range(self.ncols):
                                 ax[i, j].imshow(
-                                    (img[k].permute(1, 2, 0) * 127.5 + 128).clamp(0, 255).to(torch.uint8).detach().cpu().numpy()
+                                    (img[k].permute(1, 2, 0) * 127.5 + 128).clamp(0, 255).to(
+                                        torch.uint8).detach().cpu().numpy()
                                 )
                                 ax[i, j].text(
                                     0.0, -2.0,
@@ -300,17 +342,35 @@ class Trainer:
                                 )
                                 k += 1
 
-                        self.writer_predictions.add_figure('Real vs Pred', figure=fig, global_step=self.step)
+                        self.writer_predictions.add_figure('Validation Real vs Pred', figure=fig, global_step=self.step)
                         self.step += 1
 
-                        self.vit_model.train(True)
-                        self.mlp_head.train(True)
+                self.vit_model.train()
+                self.mlp_head.train()
 
-            # TODO: Validation loop.
+            validation_loss_value = running_validation_loss / len(self.validation_loader)
+            validation_accuracy = (100.0 * validation_correct_preds / validation_total_data)
+
+            print(
+                f"TRAINING LOSS: {train_loss_value:.3f}, "
+                f"TRAINING ACCURACY: {training_accuracy:.3f}, "
+                f"VALIDATION LOSS: {validation_loss_value:.3f}, "
+                f"VALIDATION ACCURACY: {validation_accuracy:.3f}"
+            )
+
+            if validation_accuracy > best_accuracy:
+                best_accuracy = validation_accuracy
+                torch.save({
+                    'epoch': epoch,
+                    'vit_model_state_dict': self.vit_model.state_dict(),
+                    'mlp_head_state_dict': self.mlp_head.state_dict(),
+                    'optimizer_state_dict': self.optim.state_dict(),
+                }, f'checkpoints/checkpoint_{epoch}.pt')
+
 
 if __name__ == '__main__':
     trainer = Trainer(
         dataset_path=r'C:\portable\staging\classification_data',
-        # checkpoint_path='checkpoints/checkpoint_18.pt',
+        # checkpoint_path='checkpoints/checkpoint_4.pt',
     )
     trainer.train()
