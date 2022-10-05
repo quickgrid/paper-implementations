3eaadd9a06653183c3feb69a69e4953fa258076b
Asif Ahmed <public.quickgrid@gmail.com>
2022-10-06 02:57:04 +0600

    Update test_imagen.py

--- pytorch/imagen/test_imagen.py
+++ pytorch/imagen/test_imagen.py
@@ -1,5 +1,10 @@
 import torch
-from imagen import EfficientUNetResNetBlock, EfficientUNetDBlock, TransformerEncoderSA
+from imagen import (
+    EfficientUNetResNetBlock,
+    EfficientUNetDBlock,
+    TransformerEncoderSA,
+    EfficientUNetUBlock
+)
 
 
 class Tester:
@@ -10,6 +15,7 @@ class Tester:
         self.test_efficient_unet_res_block()
         self.test_efficient_unet_dblock()
         self.test_transformer_encoder_sa()
+        self.test_efficient_unet_ublock()
 
     def test_efficient_unet_res_block(self) -> None:
         n = 4
@@ -50,7 +56,7 @@ class Tester:
             in_channels=in_channels,
             out_channels=out_channels,
             cond_embed_dim=cond_embed_dim,
-            context_embed_dim=contextual_text_embed_dim,
+            contextual_text_embed_dim=contextual_text_embed_dim,
             num_resnet_blocks=2,
             stride=stride,
             use_attention=True,
@@ -60,6 +66,36 @@ class Tester:
 
         out = efficient_unet_dblock(
             x=x, conditional_embedding=cond_embedding, contextual_text_embedding=contextual_embedding
+        )
+        print(f'In shape: {x.shape}, Out shape: {out.shape}')
+
+        assert out.shape == (n, out_channels, hw_new, hw_new), 'Error expected output shapes do not match.'
+
+    def test_efficient_unet_ublock(self):
+        n = 4
+        out_channels = 128
+        hw = 64
+        cond_embed_dim = 256
+
+        stride = (2, 2)
+        assert stride[0] == stride[1], 'Equal stride must be used.'
+        hw_new = 64 * stride[0]
+
+        x = torch.randn(size=(n, out_channels, hw, hw), device=self.device)
+        cond_embedding = torch.rand(size=(n, 1, 1, cond_embed_dim), device=self.device)
+
+        efficient_unet_ublock = EfficientUNetUBlock(
+            out_channels=out_channels,
+            cond_embed_dim=cond_embed_dim,
+            num_resnet_blocks=3,
+            stride=stride,
+            use_attention=True,
+        ).to(self.device)
+
+        print(efficient_unet_ublock)
+
+        out = efficient_unet_ublock(
+            x=x, x_skip=x, conditional_embedding=cond_embedding
         )
         print(f'In shape: {x.shape}, Out shape: {out.shape}')
 
@@ -90,3 +126,4 @@ if __name__ == '__main__':
     # tester.test_efficient_unet_res_block()
     # tester.test_efficient_unet_dblock()
     # tester.test_transformer_encoder_sa()
+    # tester.test_efficient_unet_ublock()
